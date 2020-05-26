cdc0818715c06e8dfa699d7f4c54d903babcacaf
Shubham Arora <aroras@bu.edu>
2020-05-26 11:33:45 -0400

    Dat basic file add and receive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import SDK from "dat-sdk";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <h2> P2P file share </h2>
+            <input type="file" name="file-upload" id="file-upload" accept="image/png, image/jpeg"/>
+            <button onClick={createDat} style={{margin: "5px"}}> Start Sharing </button>
+
+            <br/>
+            <input type="text" name="receive-link" id="receive-text"/>
+            <button onClick={receiveDat} style={{margin: "5px"}}> Receive files </button>
+            <div id="received-val"></div>
+        </div>
+    );
+}
+
+async function createDat() {
+    const datSdk = window.datSdk;
+
+    const { DatArchive } = datSdk;
+
+    const archive = await DatArchive.create({
+        title: 'magic-dat'
+    });
+
+    const selectedFile = document.getElementById('file-upload').files[0];
+
+    await archive.writeFile('/upload1', selectedFile);
+
+    // Open this in Beaker
+    window.datUrl = archive.url;
+    console.log(archive.url);
+}
+
+async function receiveDat() {
+    const { DatArchive } = window.datSdk;
+    // Load the archive for the dat foundation website
+    const datUrl = document.getElementById("receive-text").value;
+    console.log("reading: " + datUrl);
+    const archive = await DatArchive.load(datUrl);
+
+    // Read a file and parse it as JSON
+    const datJSON = await archive.readFile('/upload1');
+    document.getElementById("received-val").innerHTML = datJSON;
+
+    console.log(datJSON);
 }
 
 export default App;
